4ebfa831c96c041e1d4727d79246870ef9a5b7b8
ansh-kmar9 <145178376+ansh-kmar9@users.noreply.github.com>
2024-04-15 12:44:59 +0530

    index.js

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/the_sweets";
+const MONGO_URL = "mongodb+srv://anshkmr99:ansh123@cluster0.mtiofuo.mongodb.net/";
 
 main()
   .then(() => {
